88e697a2b01a0190abc5709fc9b70407fdae5fff
Liopol2 <leonardosoumas3@gmail.com>
2022-12-13 22:17:13 -0300

    Folder Selection Added

--- ytmusicdw.py
+++ ytmusicdw.py
@@ -1,21 +1,23 @@
 import tkinter as tk
+from tkinter import filedialog
 from pytube import YouTube,Playlist
 import os 
 #In case you want to change the output folder just change the next line and run the py file
 #Or install pyinstaller and run pyinstaller  -F -w ytmusicdw.py to generate a new binary on the dist folder
-musicfolder = os.path.join(os.path.expanduser('~'),'Music')
+
 
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack()
-
+        self.musicfolder = os.path.join(os.path.expanduser('~'),'Music')
         #url entry
-        self.url = tk.Entry(width=41, font='20',justify='center')
+        self.url = tk.Entry(width=31, font='Impact 30',justify='center')
         self.url.pack()
 
+        #convert the
         #destination folder Label
-        self.folder= tk.Label(text='Destino: ' + musicfolder)
+        self.folder = tk.Label(text='Destino: ' + self.musicfolder,font='25')        
         self.folder.pack()
         
         # Create the application variable.
@@ -23,7 +25,7 @@ class App(tk.Frame):
         # Set it to some value.
         self.contents.set("Pegar link y Presionar Enter")
         self.url["textvariable"] = self.contents
-
+        
         #A function that will be called on click
         def clearentry(event):
             self.url.config(fg='black')
@@ -34,26 +36,39 @@ class App(tk.Frame):
         # It prints the current value of the variable.
         self.url.bind('<Key-Return>',
                              self.validate_link)
-    
+        #bind click on route                     
+        self.folder.bind('<1>', self.clickroute)
     def validate_link(self,event):
         link=self.contents.get() 
-        if 'playlist' in link:
+        if 'playlist' in link: # khe 
             self.url.config(fg='blue')
             playlist=Playlist(self.contents.get())
             for video in playlist.videos:
                 v=video.streams.filter(only_audio=True).order_by("abr").last()
-                v.download(output_path=musicfolder)
-            self.contents.set('Playlist Descargada')
+                #fijarse si ya existe y meter break
+                v.download(output_path=self.musicfolder)
+            self.contents.set(playlist.title + ' Descargada')
         elif len(link)>= 28:
             self.url.config(fg='green')
             yt = YouTube(link)
             #Buscar audio de mas calidad
             stream = yt.streams.filter(only_audio=True).order_by("abr").last()
             #Descargar
-            stream.download(output_path=musicfolder)
+            stream.download(output_path=self.musicfolder)
             self.contents.set('Cancion Descargada')
         else:
             self.contents.set('Error intente con otro link')
+            
+    def clickroute(self,event):
+        folder=filedialog.askdirectory(initialdir = self.musicfolder,
+                                        title="Seleccione una Carpeta:"                                       
+                                        )
+        print(folder)
+        if folder != '':
+            self.folder.config(text=folder,fg='red')
+            self.musicfolder=folder
+
+
 root = tk.Tk()
 myapp = App(root)
 myapp.mainloop()
